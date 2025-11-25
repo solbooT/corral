4e2b5b2855cc85ac78cf431674cf2c606d56e811
g_code <gaspard.torterat-slanda@epita.fr>
2025-11-25 16:49:58 +0100

    fix: handle generics via typeEncoding=arguments

--- source/Corral/Driver.cs
+++ source/Corral/Driver.cs
@@ -154,8 +154,9 @@ namespace cba
             // Initialize Boogie
             Clo.clo.PrintInstrumented = true;
             Clo.clo.ProverLogFileAppend = true;
-            Clo.clo.ProverLogFilePath = "prover.log";
+            Clo.clo.ProverLogFilePath = "prover.smt2";
             Clo.clo.TheProverFactory = ProverFactory.Load("SMTLib");
+            Clo.clo.TypeEncodingMethod = CoreOptions.TypeEncoding.Arguments;
             
             // /noRemoveEmptyBlocks is needed for field refinement. It ensures that
             // we get an actual path in the program (so that we can concretize it)
